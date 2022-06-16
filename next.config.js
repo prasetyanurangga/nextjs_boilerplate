2504ed55eb5a09360d6d0b9804b496615ab53ec3
prasetyanurangga <angganurprasetya4@gmail.com>
2022-06-16 20:51:34 +0700

    🚒🚒🚒

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+};
+
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
