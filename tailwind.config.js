f1f9de135ca30103326ebe7fda8147a6beecda9b
prasetyanurangga <angganurprasetya4@gmail.com>
2022-06-16 21:41:45 +0700

    �ð

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 // eslint-disable-next-line unicorn/prefer-module
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {}
